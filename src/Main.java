3ac18ddd784c7bb257a4f60aa49f11451dfc0dbf
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-06-20 09:47:51 +0300

    add visibility and title to my frame

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //JFrame = a GUI window to add components to
+
+        JFrame frame = new JFrame(); //creates a frame
+        frame.setTitle("JFrame title goes here");
+        frame.setVisible(true); //sets the x-dimension and y-dimension of frame
+        frame.setVisible(true);  //make frame visible
+
+
+
 
     }
 }
