3074c3b3c238d94fb653a59cc5fd4c799422bded
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-06-20 22:35:09 +0300

    finalize on the code

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,18 @@ public class Main {
     public static void main(String[] args) {
         //JFrame = a GUI window to add components to
 
-        JFrame frame = new JFrame(); //creates a frame
-        frame.setTitle("JFrame title goes here"); //sets title of the frame
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of the application
-        frame.setResizable(false); //
-        frame.setSize(420,420); //sets the x-dimension and y-dimension of frame
-        frame.setVisible(true);  //make frame visible
+//        JFrame frame = new JFrame(); //creates a frame
+//        frame.setTitle("JFrame title goes here"); //sets title of the frame
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of the application
+//        frame.setResizable(false); //
+//        frame.setSize(420,420); //sets the x-dimension and y-dimension of frame
+//        frame.setVisible(true);  //make frame visible
+//
+//        ImageIcon image = new ImageIcon("2022harrier.jpg"); //create an ImageIcon
+//        frame.setIconImage(image.getImage()); //change icon of frame
+//        frame.getContentPane().setBackground(new Color(123, 50, 250));
 
-        ImageIcon image = new ImageIcon("2022harrier.jpg"); //create an ImageIcon
-        frame.setIconImage(image.getImage()); //change icon of frame
-        frame.getContentPane().setBackground(new Color(123, 50, 250));
+        MyFrame myFrame = new MyFrame();
 
 
 
